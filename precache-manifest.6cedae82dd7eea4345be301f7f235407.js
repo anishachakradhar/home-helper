fdf7f2a8be877c7e5173687b91d2106ad3ba3d68
maskeynihal <maskeynihal@gmail.com>
2020-08-29 12:24:18 +0545

    Updates

---
+++ precache-manifest.6cedae82dd7eea4345be301f7f235407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e62429f45ae669e4b2f245b68228cc",
+    "url": "/anishachakradhar/home-helper/frontend/index.html"
+  },
+  {
+    "revision": "b0b625c6838644ec3520",
+    "url": "/anishachakradhar/home-helper/frontend/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "96b6b489b8e869e9b945",
+    "url": "/anishachakradhar/home-helper/frontend/static/css/main.2b5e8d76.chunk.css"
+  },
+  {
+    "revision": "b0b625c6838644ec3520",
+    "url": "/anishachakradhar/home-helper/frontend/static/js/2.e9e22e55.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/anishachakradhar/home-helper/frontend/static/js/2.e9e22e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b6b489b8e869e9b945",
+    "url": "/anishachakradhar/home-helper/frontend/static/js/main.9ff8755e.chunk.js"
+  },
+  {
+    "revision": "a16bfa115db4f09527b8",
+    "url": "/anishachakradhar/home-helper/frontend/static/js/runtime-main.3f765197.js"
+  }
+]);
